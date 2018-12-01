eff5e7f753862494ba2c4398844e2b8170bc590a
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2018-12-01 21:07:57 +0100

    Add this.state in App.js

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,32 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Ikhsan",
+        age: 20
+      },
+      {
+        name: "Habibi",
+        age: 19
+      }
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi I am react app</h1>
         <p>This is p</p>
-        <Person name="Ikhsan" age="22" />
-        <Person name="Habibi" age="23" />
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        />
         <Person name="Ikhsan Habibi" age="24">
           Hobby: Singing
         </Person>
