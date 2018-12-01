d433d1dd668625b953e853a59dfe7e6bc1d4bb7a
Ikhsan Habibi <ikhsanhabibi48@gmail.com>
2018-12-01 20:16:42 +0100

    create react element

--- app/src/App.js
+++ app/src/App.js
@@ -3,10 +3,16 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi I am react app</h1>
-      </div>
+    // return (
+    //   <div className="App">
+    //     <h1>Hi I am react app</h1>
+    //   </div>
+    // );
+    return React.createElement(
+      "div",
+      { className: "App" },
+      React.createElement("h1", null, "work?"),
+      "hi i am react"
     );
   }
 }
